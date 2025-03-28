44595c9f135600c54e2fd5aaccf00bd651f6f2c2
Spyros Desyllas <Spyros@innovisk.global>
2025-03-28 11:50:02 +0000

    update connection string placeholder in server config

--- src/server/config.ts
+++ src/server/config.ts
@@ -3,6 +3,6 @@
  */
 export const config = {
     aiProjects: {
-        connectionString: "eastus.api.azureml.ms;3e73e1d5-0689-49b9-8457-300ceeb74eea;INNOC1-D-USE-MICROSERVICES-RGRP1;innoc1-d-use-ollie-project"
+        connectionString: "your Azure OpenAI project connection string here"
     }
 };
